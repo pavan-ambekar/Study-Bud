9ea9bbff97d1172b9a38f32fd3374a28c1e6592d
Pavan A <pavan479ec@gmail.com>
2022-05-04 23:52:31 +0530

    login, logout and flash message feature added.

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
-import imp
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -12,6 +14,31 @@ from .forms import RoomForm
 #     {'id': 3, 'name': 'Frontend developers'},
 #     {'id': 4, 'name': 'Backend developers'},
 # ]
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'username OR password does not exist')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):

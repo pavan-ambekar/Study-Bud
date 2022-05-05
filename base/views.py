cb2e9bf2be1bd5885f1178843f462b9d6150e901
Pavan A <pavan479ec@gmail.com>
2022-05-05 22:17:32 +0530

    Messaging feature added.

--- base/views.py
+++ base/views.py
@@ -1,4 +1,3 @@
-from re import U
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q
@@ -7,7 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Message, Room, Topic
 from .forms import RoomForm
 
 
@@ -21,7 +20,7 @@ from .forms import RoomForm
 
 def loginPage(request):
     page = 'login'
-    
+
     if request.user.is_authenticated:
         return redirect('home')
 
@@ -40,7 +39,7 @@ def loginPage(request):
         else:
             messages.error(request, 'username OR password does not exist')
 
-    context = {'page':page}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 
@@ -50,20 +49,21 @@ def logoutUser(request):
 
 
 def registerPage(request):
-    page='register'
-    form= UserCreationForm()
-    if request.method=='POST':
+    page = 'register'
+    form = UserCreationForm()
+    if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
-            login(request,user)
+            login(request, user)
             return redirect('home')
         else:
-            messages.error(request,'An error occurred during registration')
-    context = {'page':page, 'form':form}
+            messages.error(request, 'An error occurred during registration')
+    context = {'page': page, 'form': form}
     return render(request, 'base/login_register.html', context)
+
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -81,7 +81,18 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+    context = {'room': room, 'msg': messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 
@@ -102,10 +113,10 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-    
+
     if request.user != room.host:
         return HttpResponse('You are not allowed here!!!')
-    
+
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
         if form.is_valid:
@@ -115,18 +126,34 @@ def updateRoom(request, pk):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+
 @login_required(login_url='login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
-    
+
     if request.user != room.host:
         return HttpResponse('You are not allowed here!!!')
-    
+
     if request.method == 'POST':
         room.delete()
         return redirect('home')
 
     context = {'obj': room}
+    return render(request, 'base/delete.html', context)
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!!!')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+
+    context = {'obj': message}
     return render(request, 'base/delete.html', context)
 
 
